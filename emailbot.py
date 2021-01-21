b347dc00098722fb6731bfb9a88e134b2e2d4b92
Neutrino69 <77497096+Neutrino69@users.noreply.github.com>
2021-01-21 19:18:28 +0530

    Create emailbot.py

---
+++ emailbot.py
@@ -0,0 +1,25 @@
+import smtplib
+import threading
+#import speech_recognition as sr
+
+
+
+server  =   smtplib.SMTP('smtp.gmail.com',  587)
+server.starttls()
+server.login('*********', '*********')
+def setInterval(func,time):
+    e = threading.Event()
+    while not e.wait(time):
+        func()
+def sendMail():
+    server.sendmail('***********',
+                    '*************',
+                    '*******'
+                    )
+    print('MailSent')
+setInterval(sendMail,5)
+#you need to give your credentials to run this and allow the LESS SECURE APP feature on your gmail
+#on the sendmail function FIRST ADD YOUR GMAIL,THEN THE GMAIL OF THE RECIEVER AND THEN THE MESSAGE
+# YOU WABT TO SEND
+#remember to stop the program after you have irritated your friend  JUST CLICK ON THE RED BUTTON
+#THE PROGRAM
